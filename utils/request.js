d750284831c6d50f23c7012a780523d4835c2e1f
Meirlen <miko_982@mail.ru>
2022-07-01 18:54:43 +0600

    Update request.js

--- utils/request.js
+++ utils/request.js
@@ -9,7 +9,7 @@ async function sendData (phone, message) {
 
     try {
         //TODO: move to env
-        let response = await fetch('http://138.68.87.41:5006/webhooks/rest/webhook', {
+        let response = await fetch('http://localhost:5006/webhooks/rest/webhook', {
             method: 'POST',
             mode: 'no-cors',
             headers: {
